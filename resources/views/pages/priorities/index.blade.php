a814bcc0e8d393bd3fb8c3ea9f0c6d850b6d74e9
NamrataKasundra <namratakasundra4@gmail.com>
2020-03-13 16:59:26 +0530

    filtering

--- resources/views/pages/priorities/index.blade.php
+++ resources/views/pages/priorities/index.blade.php
@@ -29,10 +29,12 @@
             </select>
     </div>
     {{-- To show Filter icon --}}
+    <div class="filter-icon">
     <div class="form-group">
         <button class="btn btn-success" id="filter" name="filter" type="submit" style="float : left;"
             aria-hidden="true"><i class="fas fa-filter"></i>
         </button>
+    </div>
     </div>
     </form>
 
